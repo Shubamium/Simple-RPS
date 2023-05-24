0c39b417cb9d825a24243b04c600065a6e5aee50
Vincent Liussyaputra <liusvinv@gmail.com>
2023-05-24 16:21:16 +0700

    Add RPS icon

--- src/components/Slot.jsx
+++ src/components/Slot.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import styled from 'styled-components';
-
+import rock from '../assets/RPS_stone.png';
+import paper from '../assets/RPS_paper.png';
+import scissors from '../assets/RPS_scissors.png';
+import hidden from '../assets/RPS_hidden.png';
 
 const StyledSlot = styled.div`
     background: red;
@@ -17,11 +20,19 @@ const StyledSlot = styled.div`
         scale: 1.02;
     }
 `
+
+const RPS = [
+    rock,
+    paper,
+    scissors,
+    hidden
+]
 export default function Slot({data,onClick}) {
     if(data === null || data === undefined)return<></>;
     return (
         <StyledSlot onClick={()=>{onClick && onClick()}}>
-            data:{data}
+            {/* data:{data} */}
+            <img src={RPS[data]} alt="" />
         </StyledSlot>
     )
 }
